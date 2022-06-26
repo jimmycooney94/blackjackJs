1a550f87ac9df140279b5475bce06042aa25afc6
Jimmy Cooney <jimmycooney1@gmail.com>
2022-06-26 12:12:06 +0100

    restructure

--- scripts/gameplay.js
+++ scripts/gameplay.js
@@ -36,6 +36,24 @@ const shuffle = function (inputArray) {
     return shuffledDeck;
 }
 
+const calcHand = function (cards) {
+    // Get an array of card values only - need to convert a/j/q/k/ to 1/10/10/10 respectively
+    let cardNums = [];
+    let cardValue;
+    for (let card of cards) {
+        cardNums.push(parseInt(card.Value.toString().replace("a", 1).replace("j", 10).replace("q", 10).replace("k", 10)));
+    }
+    console.log(cardNums);
+    // Add non-ace cards first, then loop through aces and add 1 or 11 depending on current sum
+    const notAces = cardNums.filter(val => val !== 1);
+    let sum = notAces.reduce((partialSum, a) => partialSum + a, 0);
+    const aces = cardNums.filter(val => val === 1);
+    for (let ace of aces) { if (sum <= 10) { sum = sum + 11; } else { sum = sum + 1; } }
+    return sum;
+}
+
+// *** GAMEPLAY FUNCTIONS ***
+
 const hit = function (hitter) {
     // Get new card
     let newCard = deck.pop();
@@ -55,21 +73,4 @@ const hit = function (hitter) {
 
 const stick = function () {
     console.log("stick");
-}
-
-const calcHand = function (cards) {
-    // Get an array of card values only
-    let cardNums = [];
-    let cardValue;
-    for (let card of cards) {
-        //cardNums.push(card.Value);
-        cardNums.push(parseInt(card.Value.toString().replace("a", 1).replace("j", 10).replace("q", 10).replace("k", 10)));
-    }
-    console.log(cardNums);
-    // Add non-ace cards first, then loop through aces and add 1 or 11 depending on current sum
-    const notAces = cardNums.filter(val => val !== 1);
-    let sum = notAces.reduce((partialSum, a) => partialSum + a, 0);
-    const aces = cardNums.filter(val => val === 1);
-    for (let ace of aces) { if (sum <= 10) { sum = sum + 11; } else { sum = sum + 1; } }
-    return sum;
 }
